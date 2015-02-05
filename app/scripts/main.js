a4b72ea46d9beb11bb2b8609dbd88928da82da5d
Andrea Gutierrez <andrearaq@hotmail.com>
2015-02-05 01:25:36 +0100

    rule_required

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,54 @@
 console.log('\'Allo \'Allo!');
+$("#miFormu").validate({
+    rules: {
+        nombre: {
+            required: true
+        },
+        apellidos: {
+            required: true
+        },
+        telefono: {
+            required: true
+        },
+        email: {
+            required: true
+        },
+        dem1: {
+            required: true
+        },
+        dem2: {
+            required: true
+        },
+        cif: {
+            required: true
+        },
+        nom_dem: {
+            required: true
+        },
+        direccion: {
+            required: true
+        },
+        cp: {
+            required: true
+        },
+        localidad: {
+            required: true
+        },
+        provincia: {
+            required: true
+        },
+        pais: {
+            required: true
+        },
+        iban: {
+            required: true
+        },
+        usuario: {
+            required: true
+        },
+        password: {
+            required: true
+        }
+    });
+
+
